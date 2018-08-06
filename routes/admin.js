40a5d76d93587518fbf3c52994f9b03ddb5d2d6d
곽대용 <dayong7387@hanmail.net>
2018-08-06 16:04:23 +0900

    de

--- routes/admin.js
+++ routes/admin.js
@@ -33,7 +33,15 @@ var noticeupload = multer({storage: noticestorage});
 const op = Sequelize.Op
 
 Date.prototype.dateformat = function () {
+    var yyyy = this.getFullYear().toString();
+    var MM = pad(this.getMonth() + 1, 2);
+    var dd = pad(this.getDate(), 2);
+    this.setHours(this.getHours());
+    var hh = pad(this.getHours(), 2);
+    var mm = pad(this.getMinutes(), 2)
+    var ss = pad(this.getSeconds(), 2)
 
+    return yyyy + MM + dd + hh + mm + ss;
 };
 
 function pad(number, length) {
